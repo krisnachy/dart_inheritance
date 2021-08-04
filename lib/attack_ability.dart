01e554862943d84e89861cfe4bc880a61bda2137
krisnachy <cahyayuana@gmail.com>
2021-08-04 20:53:57 +0700

    Using on

--- lib/attack_ability.dart
+++ lib/attack_ability.dart
@@ -1,3 +1,5 @@
+//Jika hanya ingin cuma bisa digunakan oleh class tertentu dan turunannya gunakan on
+//example: mixin AttackAbility on Hero
 mixin AttackAbility {
   String attack() => "Slassshhhhhh";
 }
